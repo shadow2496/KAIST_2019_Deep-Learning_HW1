3eae793a4fa3ccbc0d2d801eea56eb2ba24513bd
Seunghwan Choi <dadadak4@gmail.com>
2019-10-21 03:40:15 +0900

    Add FCN.optimizer

--- config.py
+++ config.py
@@ -7,6 +7,10 @@ config.dataset_dir = './datasets/'
 config.tensorboard_dir = './tensorboard/'
 config.checkpoint_dir = './checkpoints/'
 
+config.lr = 1e-4
+config.momentum = 0.9
+config.weight_decay = 1.6e-3
+
 config.load_iter = 0
 config.train_iters = 0
 config.is_train = True
--- models.py
+++ models.py
@@ -1,34 +1,7 @@
-import numpy as np
-import torch
 from torch import nn, optim
 import torchvision.models
 
-
-# https://github.com/shelhamer/fcn.berkeleyvision.org/blob/master/surgery.py
-def get_upsampling_weight(in_channels, out_channels, kernel_size):
-    """Make a 2D bilinear kernel suitable for upsampling"""
-    factor = (kernel_size + 1) // 2
-    if kernel_size % 2 == 1:
-        center = factor - 1
-    else:
-        center = factor - 0.5
-    og = np.ogrid[:kernel_size, :kernel_size]
-    filt = (1 - abs(og[0] - center) / factor) * \
-           (1 - abs(og[1] - center) / factor)
-    weight = np.zeros((in_channels, out_channels, kernel_size, kernel_size),
-                      dtype=np.float64)
-    weight[range(in_channels), range(out_channels), :, :] = filt
-    return torch.from_numpy(weight).float()
-
-
-def initialize_weights(m):
-    if isinstance(m, nn.Conv2d):
-        nn.init.zeros_(m.weight)
-        if m.bias is not None:
-            nn.init.zeros_(m.bias)
-    elif isinstance(m, nn.ConvTranspose2d):
-        initial_weight = get_upsampling_weight(m.in_channels, m.out_channels, m.kernel_size[0])
-        m.weight.data.copy_(initial_weight)
+from utils import initialize_weights
 
 
 class FCN32s(nn.Module):
@@ -64,12 +37,11 @@ class FCN(nn.Module):
 
         self.is_train = config.is_train
         vgg16 = torchvision.models.vgg16(pretrained=self.is_train)
-        for param in vgg16.features.parameters():
-            param.required_grad = False
         self.network = FCN32s(vgg16.features)
 
         if self.is_train:
-            pass
+            self.optimizer = optim.SGD(self.network.parameters(), lr=config.lr,
+                                       momentum=config.momentum, weight_decay=config.weight_decay)
 
     def forward(self, x):
         return self.network(x)
