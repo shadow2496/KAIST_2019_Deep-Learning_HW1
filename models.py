2710cd34cc039c0c0661a26cda165f6854187784
Seunghwan Choi <dadadak4@gmail.com>
2019-10-21 00:33:30 +0900

    Add get_upsampling_weight()

--- models.py
+++ models.py
@@ -4,6 +4,23 @@ from torch import nn, optim
 import torchvision.models
 
 
+# https://github.com/shelhamer/fcn.berkeleyvision.org/blob/master/surgery.py
+def get_upsampling_weight(in_channels, out_channels, kernel_size):
+    """Make a 2D bilinear kernel suitable for upsampling"""
+    factor = (kernel_size + 1) // 2
+    if kernel_size % 2 == 1:
+        center = factor - 1
+    else:
+        center = factor - 0.5
+    og = np.ogrid[:kernel_size, :kernel_size]
+    filt = (1 - abs(og[0] - center) / factor) * \
+           (1 - abs(og[1] - center) / factor)
+    weight = np.zeros((in_channels, out_channels, kernel_size, kernel_size),
+                      dtype=np.float64)
+    weight[range(in_channels), range(out_channels), :, :] = filt
+    return torch.from_numpy(weight).float()
+
+
 class FCN32s(nn.Module):
     def __init__(self, features):
         super(FCN32s, self).__init__()
