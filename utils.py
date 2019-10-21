f55d18f8fa146282b03e67d43d6644917d6381b6
Seunghwan Choi <dadadak4@gmail.com>
2019-10-21 23:45:27 +0900

    Add label_accuracy_score()

--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ from torch import nn
 
 def load_checkpoints(network, name, checkpoint_dir, load_iter):
     filename = '{:06d}-{}.ckpt'.format(load_iter, name)
-    print('Loading {}...'.format(filename))
+    print("Loading {}...".format(filename))
     network_path = os.path.join(checkpoint_dir, filename)
     network.load_state_dict(torch.load(network_path, map_location=lambda storage, loc: storage))
 
@@ -57,3 +57,37 @@ def get_colormap(n=256):
         cmap[i, 1] = g / 255
         cmap[i, 2] = b / 255
     return cmap
+
+
+# https://github.com/wkentaro/pytorch-fcn/blob/master/torchfcn/utils.py
+def _fast_hist(label_true, label_pred, n_class):
+    mask = (label_true >= 0) & (label_true < n_class)
+    hist = np.bincount(
+        n_class * label_true[mask].astype(int) +
+        label_pred[mask], minlength=n_class ** 2).reshape(n_class, n_class)
+    return hist
+
+
+# https://github.com/wkentaro/pytorch-fcn/blob/master/torchfcn/utils.py
+def label_accuracy_score(label_trues, label_preds, n_class):
+    """Returns accuracy score evaluation result.
+      - overall accuracy
+      - mean accuracy
+      - mean IU
+      - fwavacc
+    """
+    hist = np.zeros((n_class, n_class))
+    for lt, lp in zip(label_trues, label_preds):
+        hist += _fast_hist(lt.flatten(), lp.flatten(), n_class)
+    acc = np.diag(hist).sum() / hist.sum()
+    with np.errstate(divide='ignore', invalid='ignore'):
+        acc_cls = np.diag(hist) / hist.sum(axis=1)
+    acc_cls = np.nanmean(acc_cls)
+    with np.errstate(divide='ignore', invalid='ignore'):
+        iu = np.diag(hist) / (
+            hist.sum(axis=1) + hist.sum(axis=0) - np.diag(hist)
+        )
+    mean_iu = np.nanmean(iu)
+    freq = hist.sum(axis=1) / hist.sum()
+    fwavacc = (freq[freq > 0] * iu[freq > 0]).sum()
+    return acc, acc_cls, mean_iu, fwavacc
